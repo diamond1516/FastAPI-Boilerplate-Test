ecc4102e6f46146fbc2a31a7c1d32fafdbad54c8
user <ahmadboyabdurahimov589@gmail.com>
2024-06-13 17:16:47 +0500

    new changes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.api.api import api_router
-from app.core.config import settings
+from app.core.config import SETTINGS
 
-app = FastAPI(title=settings.PROJECT_NAME)
+app = FastAPI(title=SETTINGS.PROJECT_NAME)
 
-app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(api_router, prefix=SETTINGS.API_V1_STR)
