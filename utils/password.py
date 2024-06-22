c558a4db73dac7e600b204728e2295b63a4818ae
user <ahmadboyabdurahimov589@gmail.com>
2024-06-22 13:08:15 +0500

    new changes

--- utils/password.py
+++ utils/password.py
@@ -1,13 +1,13 @@
 import bcrypt
 
 
-def hash_password(password) -> bytes:
+async def hash_password(password) -> bytes:
     salt = bcrypt.gensalt()
     pwd_bytes: bytes = password.encode('utf-8')
     return bcrypt.hashpw(pwd_bytes, salt)
 
 
-def verify_password(password: str, hashed_password: bytes) -> bool:
+async def verify_password(password: str, hashed_password: bytes) -> bool:
     return bcrypt.checkpw(
         password.encode('utf-8'),
         hashed_password
