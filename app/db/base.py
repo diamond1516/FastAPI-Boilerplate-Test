5add943c247fa1af4052f9042475d946ad23fcf2
user <ahmadboyabdurahimov589@gmail.com>
2024-07-15 15:14:34 +0500

    add new changes on the utility.py

--- app/db/base.py
+++ app/db/base.py
@@ -6,6 +6,7 @@ class Base(DeclarativeBase):
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
-        return f"{cls.__name__.lower()}s"
+        name = str(cls.__name__)
+        return ''.join(['_' + c.lower() if c.isupper() else c for c in name]).lstrip('_') + 's'
 
     id: Mapped[int] = mapped_column(primary_key=True)
