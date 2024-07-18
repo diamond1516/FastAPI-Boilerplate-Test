1ef9f25585303fdd08a9ccbc244ca0eb273078ef
user <ahmadboyabdurahimov589@gmail.com>
2024-07-18 11:48:58 +0500

    add new changes on the utility.py

--- utils/customs/fields.py
+++ utils/customs/fields.py
@@ -1,13 +1,13 @@
 from sqlalchemy import String
-from fastapi.datastructures import UploadFile
+from starlette.datastructures import UploadFile
+from sqlalchemy.types import TypeDecorator
 
 import os
-from abc import ABC, abstractmethod
 from werkzeug.utils import secure_filename
 
 
 class StorageManager:
-    def save(self, file):
+    def save(self, file, upload_folder):
         raise NotImplementedError
 
     def get_url(self, filename):
@@ -20,9 +20,9 @@ class StorageManager:
 class LocalStorageManager(StorageManager):
     upload_folder = None
 
-    def save(self, file):
+    def save(self, file, upload_folder):
         filename = secure_filename(file.filename)
-        file_path = os.path.join(self.upload_folder, filename)
+        file_path = os.path.join(upload_folder, filename)
         with open(file_path, 'wb') as f:
             f.write(file.file.read())
         return file_path
@@ -31,33 +31,38 @@ class LocalStorageManager(StorageManager):
         return os.path.join(self.upload_folder, filename)
 
 
-class FileField(String):
-    def __init__(self, storage_manager, upload_folder, *args, **kwargs):
-        self.storage_manager = storage_manager()
-        self.storage_manager.upload_folder = upload_folder
+class FileField(TypeDecorator):
+    impl = String
 
+    def __init__(self, storage_manager, upload_folder, *args, **kwargs):
+        self.storage_manager = storage_manager()  # Storage manager obyektini yaratamiz
+        self.storage_manager.upload_folder = upload_folder
         self.upload_folder = upload_folder
         super().__init__(*args, **kwargs)
 
     def process_bind_param(self, value, dialect):
+        print(value)
+        print(self.upload_folder)
+        print(self.storage_manager)
         if isinstance(value, UploadFile):
             file_path = self.storage_manager.save(value)
+            print(file_path)
             return file_path
-        return value
+        return str(value)
 
     def process_result_value(self, value, dialect):
         if value:
-            return open(value, 'rb')
-        return value
+            return open(value, 'rb')  # Faylni o'qib olish
+        return value  # Qiymatni o'zgarmas qaytaradi
 
     def file_name(self, value):
-        return os.path.basename(value)
+        return os.path.basename(value) if value else None  # Fayl nomini qaytaradi
 
     def file_url(self, value):
-        return self.storage_manager.get_url(value)
+        return self.storage_manager.get_url(value) if value else None  # Fayl URL sini qaytaradi
 
     def file_path(self, value):
-        return self.storage_manager.get_path(value, self.upload_folder)
+        return self.storage_manager.get_path(value, self.upload_folder) if value else None  # Fayl manzilini qaytaradi
 
     def file_extension(self, value):
-        return os.path.splitext(value)[1]
+        return os.path.splitext(value)[1] if value else None  # Fayl kengaytmasini qaytaradi
