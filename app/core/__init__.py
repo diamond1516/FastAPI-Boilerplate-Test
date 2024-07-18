5c9a9c0b22fa2a075951c5a8f6625af82a2974ad
user <ahmadboyabdurahimov589@gmail.com>
2024-07-18 16:49:19 +0500

    add new changes on the utility.py

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -5,11 +5,9 @@ __all__ = (
     'DB_SETTINGS',
     'MAIN_SECURITY',
     'DB_SECURITY',
-    'LOCAL_STORAGE',
 )
 
 from app.core.config import SETTINGS, DB_SETTINGS
 from app.core.security import MAIN_SECURITY, DB_SECURITY
 from app.core.routes import Routes
 from app.core.server import Server
-from app.core.storages import LOCAL_STORAGE
